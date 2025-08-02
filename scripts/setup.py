5d9bf24df4585511ec51c0d4e14109ec466572fa
tanntnny <115852308+tanntnny@users.noreply.github.com>
2025-08-03 00:56:27 +0700

    update

--- scripts/setup.py
+++ scripts/setup.py
@@ -1,4 +1,7 @@
 from transformers import Wav2Vec2Processor, Wav2Vec2Model
 
-Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base", cache_dir="models/wav2vec2-processor")
-Wav2Vec2Model.from_pretrained("facebook/wav2vec2-base", cache_dir="models/wav2vec2-model")
+processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base")
+processor.save_pretrained("models/wav2vec2-processor")
+
+model = Wav2Vec2Model.from_pretrained("facebook/wav2vec2-base")
+model.save_pretrained("models/wav2vec2-model")
