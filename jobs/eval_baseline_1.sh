f224fbc42ae64eed05647f05afddfc80215d3e76
tanntnny <115852308+tanntnny@users.noreply.github.com>
2025-08-07 14:24:12 +0700

    Update eval_baseline_1.pt

--- jobs/eval_baseline_1.sh
+++ jobs/eval_baseline_1.sh
@@ -19,11 +19,11 @@ conda activate pytorch-2.2.2
 
 export PYTHONPATH=$PWD
 
-DATA_CONFIG=datasets/SM/ICNALE_SM_Audio/val_config.csv
+VAL_CONFIG=datasets/SM/ICNALE_SM_Audio/val_config.csv
 MODEL_PATH=assets/weights_baseline_1.pt
 SAVE_PATH=runs/predictions.json
 
 python scripts/eval_baseline_1.py \
-    --data_config "$DATA_CONFIG" \
+    --val_config "$VAL_CONFIG" \
     --model_path "$MODEL_PATH" \
     --save_path "$SAVE_PATH"
