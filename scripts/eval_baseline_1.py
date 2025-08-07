7d548911b2934f27a421683da9fedc971e376237
tanntnny <115852308+tanntnny@users.noreply.github.com>
2025-08-07 14:19:30 +0700

    Update eval_baseline_1.pt

--- scripts/eval_baseline_1.py
+++ scripts/eval_baseline_1.py
@@ -3,8 +3,6 @@ import os
 import torch
 import pandas as pd
 import numpy as np
-from sklearn.metrics import confusion_matrix
-import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader
 from transformers import Wav2Vec2Processor
 import json
