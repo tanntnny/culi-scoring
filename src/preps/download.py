5f7e45ae9fa2bd6ad54051f64060cee6b26679e5
tanntnny <115852308+tanntnny@users.noreply.github.com>
2025-10-06 00:28:38 +0700

    update

--- src/preps/download.py
+++ src/preps/download.py
@@ -4,5 +4,6 @@ from ..registry import build
 
 # ---------------- Download ----------------
 def run_download(cfg):
+    print(cfg.download)
     downloader = build("downloader", cfg.download.name, cfg)
     downloader.download()
