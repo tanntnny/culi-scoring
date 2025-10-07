e5de149799df8419bdc6393df6d68d2552fad4d6
tanntnny <115852308+tanntnny@users.noreply.github.com>
2025-10-07 13:42:38 +0700

    update

---
+++ src/preps/phi4_download.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+from transformers import AutoModelForCausalLM, AutoProcessor
+
+from pathlib import Path
+
+from .base import BaseDownloader
+from ..utils.io import save_checkpoint
+from ..registry import register
+
+# ---------------- Phi4 Downloader ----------------
+class Phi4Downloader(BaseDownloader):
+    def __init__(self, cfg):
+        self.cfg = cfg
+        
+    def download(self):
+        print(f"Downloading the Phi-4 model and processor ...")
+        save = Path(self.cfg.download.save)
+        model = AutoModelForCausalLM.from_pretrained(
+            self.cfg.download.model,
+            torch_dtype="auto",
+            device_map="auto"
+        )
+        processor = AutoProcessor.from_pretrained(self.cfg.download.model)
+        print("Model loaded:", model.config.model_type)
+
+        model.save_pretrained(save / "phi4-model")
+        processor.save_pretrained(save / "phi4-processor")
+        print(f"Download complete.")
+
+@register("downloader", "phi4")
+def build_phi4_downloader(cfg):
+    downloader = Phi4Downloader(cfg)
+    return downloader
