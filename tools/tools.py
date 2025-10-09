491de237b9769fb2b1702ea0fda5a150f776c561
tanntnny <115852308+tanntnny@users.noreply.github.com>
2025-10-09 14:36:11 +0700

    update

--- tools/tools.py
+++ tools/tools.py
@@ -4,7 +4,7 @@ import argparse
 
 _useful_commands = (
     ("python3 -m tools.tools", "Show this help message"),
-    ("python3 -m tools.monitor --ext ['.err', '.out']  --select latest --n 1 --cat", "Show latest .err and .out files"),
+    ("python3 -m tools.monitor --ext .err .out  --select latest --n 1 --cat", "Show latest .err and .out files"),
     ("python3 -m tools.monitor --select latest --n 5", "Show 5 most recent log files under outputs/ and logs/"),
     ("python3 -m tools.monitor --select oldest --n 5", "Show 5 oldest log files under outputs/ and logs/"),
     ("python3 -m tools.eda --src path/to/your.csv", "Show basic statistics of a CSV file"),
