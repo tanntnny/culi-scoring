9f0e1e6eec2aebbfe43fa76b6b029ac0f3cb0901
tanntnny <115852308+tanntnny@users.noreply.github.com>
2025-10-14 00:14:12 +0700

    LoRA Finetuning

--- src/models/phi4.py
+++ src/models/phi4.py
@@ -1,7 +1,5 @@
 import torch.nn as nn
-from transformers import AutoModelForCausalLM, AutoConfig
-from peft import get_peft_model, LoraConfig
-import types
+from transformers import AutoModelForCausalLM
 from dataclasses import dataclass
 from typing import Any
 
@@ -17,42 +15,21 @@ class Phi4ModelConfig:
 
 # ---------------- Phi4 Model ----------------
 class Phi4ScorerModel(nn.Module):
+    
     def __init__(
             self,
             cfg
     ):
         super().__init__()
         self.config = Phi4ModelConfig(**cfg.model)
-        config = AutoConfig.from_pretrained(self.config.src, trust_remote_code=True)
-        vision_peft_config = config.vision_peft_config
-        speech_peft_config = config.speech_peft_config
-        config.vision_peft_config = None
-        config.speech_peft_config = None
-
         self.model = AutoModelForCausalLM.from_pretrained(
             self.config.src,
-            config=config,
             trust_remote_code=True,
             torch_dtype=self.config.torch_dtype,
             attn_implementation=self.config.attn_implementation
         )
-
-        def prepare_inputs_for_generation_patch(self, *args, **kwargs):
-            return self.model.prepare_inputs_for_generation(*args, **kwargs)
-
-        self.model.model.prepare_inputs_for_generation = types.MethodType(
-            prepare_inputs_for_generation_patch, self.model.model
-        )
-
-        if vision_peft_config:
-            vision_lora_config = LoraConfig(**vision_peft_config)
-            self.model.model = get_peft_model(self.model.model, vision_lora_config, adapter_name="vision")
-
-        if speech_peft_config:
-            speech_lora_config = LoraConfig(**speech_peft_config)
-            self.model.model = get_peft_model(self.model.model, speech_lora_config, adapter_name="speech")
-
         self.model.set_lora_adapter("speech")
+
     def forward(
             self,
             x: Any
