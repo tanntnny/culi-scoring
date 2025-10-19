e28526fa417d470f749ceeff46eb798b71ea71df
tanntnny <115852308+tanntnny@users.noreply.github.com>
2025-10-19 23:44:26 +0700

    Fix HFTRAINER

--- scripts/experiment/hf_finetune_lora.sh
+++ scripts/experiment/hf_finetune_lora.sh
@@ -57,4 +57,4 @@ mkdir -p "$HF_HOME" "$HF_DATASETS_CACHE" "$TORCH_HOME" "$WANDB_DIR" "$XDG_CACHE_
 
 # ---------------- Launch HuggingFace Trainer ----------------
 srun --kill-on-bad-exit=1 --gpu-bind=none \
-  python -m src.main cmd=hftrain train=hftrain data=phi4 model=phi4 task=finetune_lora ddp=True
+  python -m src.main cmd=hftrain train=hf_train data=phi4 model=phi4 task=finetune_lora ddp=True
